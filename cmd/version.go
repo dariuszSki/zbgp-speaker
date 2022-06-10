b650e3f33bd242b89aae77dcf6966fe42f21ffac
Dariusz Sliwinski <dariusz.sliwinski@netfoundry.io>
2022-06-10 16:30:33 -0400

    Update version.go

--- cmd/version.go
+++ cmd/version.go
@@ -12,7 +12,7 @@ func init() {
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version number of zgbp",
+	Short: "Print the version number of iptables-bgp-scraper",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("0.1.0")
 	},
